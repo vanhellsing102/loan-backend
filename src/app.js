d7364ef256eda5bdda4944f63782ede8e589390e
vanhellsing102 <vvanhellsing30@gmail.com>
2025-09-09 22:21:21 -0600

    server and config file for dotenv

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+import express from 'express';
+import cookieParser from 'cookie-parser';
 const app = express();
-const port = process.env.PORT || 3000;
+
 
 app.use(express.json());
 app.use(cookieParser());
--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
+import app from "./app.js";
+import mongoose from "mongoose";
+import config from "./app/config/index.js";
 
 let server;
 
-async function main(){
-    try{
-        server = app.listen(port, () =>{
-        console.log(`Server is running on port ${port}`);
-        });
-    }catch(err){
-        console.log(err);
-    }
+async function main() {
+  try {
+    await mongoose.connect(config.databaseUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("Database connected");
+    server = app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log("Error connecting to the database:", err);
+  }
 }
 main();
