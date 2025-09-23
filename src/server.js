6692fb63bbc03092ec5de867645467b065976459
vanhellsing102 <vvanhellsing30@gmail.com>
2025-09-22 22:49:20 -0600

    port customize

--- src/server.js
+++ src/server.js
@@ -6,14 +6,15 @@ let server;
 
 async function main() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 4000, { //config.databaseUrl
+    await mongoose.connect(process.env.MONGODB_URI, { //config.databaseUrl
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
     console.log("Database connected");
-    // server = app.listen(process.env.PORT, () => { //config.port
-    //   console.log(`Server is running on port ${process.env.PORT}`);
-    // });
+    const port = process.env.PORT;
+    server = app.listen(port, "0.0.0.0", () => { //config.port
+      console.log(`Server is running on port ${port}`);
+    });
   } catch (err) {
     console.log("Error connecting to the database:", err);
   }
