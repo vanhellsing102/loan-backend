b32b18367175e9ad50fab017d6e1b8a69501d733
vanhellsing102 <vvanhellsing30@gmail.com>
2025-09-22 22:57:01 -0600

    api change

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173', "http://localhost:3000"],
+    origin: ['http://localhost:5173', "https://loan-one-mu.vercel.app"],
     credentials: true
 }))
 
